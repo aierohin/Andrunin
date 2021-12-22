e98e8270a18a562169f07323a5f2f922d2d44826
Sergey Andryunin <s.andryunin@Sergejs-MacBook-Pro.local>
2021-12-22 23:40:12 +0300

    add workspace support, new vpc module with folder auto creation

--- terraform/demo/vars.tf
+++ terraform/demo/vars.tf
@@ -1,25 +1,16 @@
 variable "yc_token" {
    default = ""
- }
+}
+
 variable "yc_cloud_id" {
   default = ""
 }
+
 variable "yc_folder_id" {
-  default = "b1g0js1146c55lamm0g7"
+  default = ""
 }
+
 variable "yc_region" {
-  default = "ru-central1-a"
-}
-variable "yc_project_name" {
-  default = "default"
-}
-variable "yc_network_id" {
-  default = "enp09opd7ksaedcorclb"
-}
-variable "yc_subnet_id" {
-  default = "e9bjnga63shpd7873vq0"
-}
-variable "yc_zone" {
   default = "ru-central1-a"
 }
 
--- terraform/modules/instance/instance.tf
+++ terraform/modules/instance/instance.tf
@@ -9,10 +9,13 @@ variable memory { default = ""}
 variable core_fraction { default = "20"}
 variable subnet_id { default = ""}
 variable nat { default = "false"}
+variable instance_count { default = 1 }
 variable count_offset { default = 0 } #start numbering from X+1 (e.g. name-1 if '0', name-3 if '2', etc.)
 variable count_format { default = "%01d" } #server number format (-1, -2, etc.)
 variable boot_disk { default =  "network-hdd" }
 variable disk_size { default =  "20" }
+variable zone { default =  "" }
+variable folder_id { default =  "" }
 
 terraform {
   required_providers {
@@ -24,26 +27,28 @@ terraform {
 }
 
 data "yandex_compute_image" "image" {
-  family = "${var.image}"
+  family = var.image
 }
 
 resource "yandex_compute_instance" "instance" {
-  name = "${var.name}" #-${format(var.count_format, var.count_offset+count.index+1)}"
-  platform_id = "${var.platform_id}"
-  hostname = "${var.name}"
-  description = "${var.description}"
-
+  count = var.instance_count
+  name = "${var.name}-${format(var.count_format, var.count_offset+count.index+1)}"
+  platform_id = var.platform_id
+  hostname = "${var.name}-${format(var.count_format, var.count_offset+count.index+1)}"
+  description = var.description
+  zone = var.zone
+  folder_id = var.folder_id
 
   resources {
-    cores  = "${var.cores}"
-    memory = "${var.memory}"
-    core_fraction = "${var.core_fraction}"
+    cores  = var.cores
+    memory = var.memory
+    core_fraction = var.core_fraction
   }
   boot_disk {
     initialize_params {
-      image_id = "${data.yandex_compute_image.image.id}"
-      type = "${var.boot_disk}"
-      size = "${var.disk_size}"
+      image_id = data.yandex_compute_image.image.id
+      type = var.boot_disk
+      size = var.disk_size
     }
   }
   network_interface {
